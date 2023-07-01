290b7cf8a11758ee85309fdc57912c8175eb41b4
SaitamaGoku <136069394+SaitamaGoku@users.noreply.github.com>
2023-07-01 22:40:22 +0530

    Update date_time.js
    Currently works without input

--- Pranav_Nampally/Medium/date_time.js
+++ Pranav_Nampally/Medium/date_time.js
@@ -3,3 +3,10 @@
 // Question: Write a JavaScript function that takes a date as input and returns the number of days remaining in the current month.
 
 // For example, if the input date is June 15, 2023, the function should return 15, as there are 15 days remaining in the month of June.
+
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+let currentDay = currentDate.getDate();
+let daysInCurrentMonth = new Date(currentYear,currentMonth,0).getDate();
+console.log("The number of remaining Days in this month: "+(daysInCurrentMonth - currentDay));
